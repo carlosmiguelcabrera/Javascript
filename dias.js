044f74d0d0b9bffb7acbc775a2eca5424a7b831c
carlosmiguelcabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-15 08:45:23 -0300

    Create dias.js

---
+++ dias.js
@@ -0,0 +1,29 @@
+// Días de la semana
+var day = "Domingo";
+
+switch (day) {
+  case "Lunes":
+    console.log("Ir a trabajar");
+    break;
+  case "Martes":
+    console.log("Reunión de equipo");
+    break;
+  case "Miércoles":
+    console.log("Proyecto de desarrollo");
+    break;
+  case "Jueves":
+    console.log("Presentación de informe");
+    break;
+  case "Viernes":
+    console.log("Prepararse para el fin de semana");
+    break;
+  case "Sábado":
+    console.log("Disfrutar del tiempo libre");
+    break;
+  case "Domingo":
+    console.log("Relajarse y descansar");
+    break;
+  default:
+    console.log("No existe ese día");
+    break;
+}
