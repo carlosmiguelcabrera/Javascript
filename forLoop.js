96018cfba2a48e87f32a95012c43ab10c9f30eae
carlosmiguelcabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-15 09:08:15 -0300

    Update forLoop.js

--- forLoop.js
+++ forLoop.js
@@ -1,3 +1,7 @@
+//var i = 1; determina el numero inicial.
+//i<=3; determina hasta que numero hay que contar.
+//i++ determina que se incremente el valor de a 1.
+
 for (var i = 1; i<=3; i++) {
     console.log(i)
 }
