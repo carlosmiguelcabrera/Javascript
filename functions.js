c22062e6846cc7a87ee3f3b4e4ab3f3c15c2236a
carlosmiguelcabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-21 09:04:59 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// Ejemplo 3, 2, 1 corte
 let counter = 3;
 function example() {
 console.log(counter);
@@ -5,3 +6,12 @@ counter = counter - 1;
 if (counter === 0) return;
 example();
 }
+
+// Ejemplo Funcion Programacion funcional
+function getTotal(a,b) {
+    return a + b
+}
+var num1 = 2;
+var num2 = 3;
+
+var total = getTotal(num1, num2);
