13d5719eb01758e0a6d5f3083c6eb6401c13e08c
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-26 22:40:06 -0300

    Actualizar objets.js

--- objets.js
+++ objets.js
@@ -1,2 +1,19 @@
+// Definición de una clase 'Persona'
+class Persona {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
 
-var result = "Hello".indexOf('l');
+  saludar() {
+    console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+  }
+}
+
+// Creación de objetos utilizando la clase 'Persona'
+const persona1 = new Persona('Juan', 25);
+const persona2 = new Persona('María', 30);
+
+// Llamada al método 'saludar()' de los objetos
+persona1.saludar(); // Imprime: Hola, mi nombre es Juan y tengo 25 años.
+persona2.saludar(); // Imprime: Hola, mi nombre es María y tengo 30 años.
