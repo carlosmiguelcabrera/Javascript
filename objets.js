8ce282fbfb2767b9c471a499a6538db272fb40c8
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-26 22:40:49 -0300

    Actualizar objets.js

--- objets.js
+++ objets.js
@@ -17,3 +17,30 @@ const persona2 = new Persona('María', 30);
 // Llamada al método 'saludar()' de los objetos
 persona1.saludar(); // Imprime: Hola, mi nombre es Juan y tengo 25 años.
 persona2.saludar(); // Imprime: Hola, mi nombre es María y tengo 30 años.
+
+// Definición de una clase 'Animal'
+class Animal {
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+
+  comer() {
+    console.log(`${this.nombre} está comiendo.`);
+  }
+}
+
+// Definición de una subclase 'Perro' que hereda de 'Animal'
+class Perro extends Animal {
+  ladrar() {
+    console.log(`${this.nombre} está ladrando.`);
+  }
+}
+
+// Creación de objetos utilizando las clases 'Animal' y 'Perro'
+const animal1 = new Animal('Animal');
+const perro1 = new Perro('Firulais');
+
+// Llamada a los métodos de los objetos
+animal1.comer(); // Imprime: Animal está comiendo.
+perro1.comer();  // Imprime: Firulais está comiendo.
+perro1.ladrar(); // Imprime: Firulais está ladrando.
