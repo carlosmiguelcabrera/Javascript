7cf4a2b2c828410e6790e448fd6ea84b4c9a5846
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-31 12:01:31 -0300

    Create matricesyobjetos.js

---
+++ matricesyobjetos.js
@@ -0,0 +1,37 @@
+/* Ejercicio: Creación de matrices y objetos
+
+En este ejercicio de laboratorio  practicará la creación de matrices y objetos.
+Tareas para completar
+
+    Cree un nuevo literal de matriz vacío y asígnelo a la variable ropa.
+
+    Agregue 5 de sus prendas favoritas como cadenas usando el método push().
+
+    Retire la quinta prenda de la matriz usando el método pop().
+
+    Agregue una nueva prenda usando el método push() .
+
+    Use console.log para mostrar el tercer elemento de la matriz de ropa en la consola.
+
+    Cree un nuevo literal de objeto vacío y asígnelo a la variable favCar.
+
+    Usando la notación de puntos, asigne una propiedad de color al objeto favCar y asígnele un valor de cadena con el color de su elección.
+
+    Usando la notación de puntos, asigne una propiedad convertible al objeto y favCar y asígnele un valor booleano de su elección.
+
+    Utilice la consola para registrar todo el objeto favCar.*/
+
+const ropa[];
+ropa.push('camisa');
+ropa.push('pantaton');
+ropa.push('remera');
+ropa.push('zapatillas');
+ropa.push('gorro');
+ropa.pop();
+ropa.push('sandalias');
+console.log(ropa[3]);
+
+const favCar[];
+favCar.color('Rojo');
+favCar.convertible(True);
+console.log(FavCar);
