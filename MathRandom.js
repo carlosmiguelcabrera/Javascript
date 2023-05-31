93aec261b9c4d9e28f65c5c7b38470ec73144d92
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-31 11:03:17 -0300

    Create MathRandom.js

---
+++ MathRandom.js
@@ -0,0 +1,10 @@
+// Math.random()
+
+// Generate a decimal number between 0 and 0.99
+Math.random();
+
+//Log the value of decimal to the console
+console.log(decimal);
+
+//Log the value of decimal MULTIPLIED BY 10
+console.log(decimal * 10);
