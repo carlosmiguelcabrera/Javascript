a348f3be9103b50b71d6651053785f51fd8115e5
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-31 11:16:03 -0300

    Update RamdomIntegers.js

---
+++ RamdomIntegers.js
@@ -0,0 +1,10 @@
+// Random integer
+
+// Get random decimal number between 0 and 0.99...and save it to a variable
+var decimal = Marth.random() * 10;
+
+// Rounding up the value of decimal
+var rounded = Math.ceil(decimal);
+
+//Log the value of rounded to the console
+console.log(rounded);
