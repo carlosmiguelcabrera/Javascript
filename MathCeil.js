49b01c42401329e46e4c95304bc5555f62e75ad9
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-05-31 11:11:48 -0300

    Update MathCeil.js

---
+++ MathCeil.js
@@ -0,0 +1,11 @@
+// math.ceil()
+
+//Round up any decimal number to the nerlest integer
+//var rounded = Marth.ceil(0.0001); // 1
+//var rounded = Marth.ceil(0.5);    // 1
+//var rounded = Marth.ceil(0.99);   // 1
+//var rounded = Marth.ceil(1.01);   // 2
+//var rounded = Marth.ceil(1.5);    // 2
+//var rounded = Marth.ceil(2.99);   // 3
+
+console.log(rounded);
